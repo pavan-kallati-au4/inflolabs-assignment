db4cddf84cdd603f395aa090868db1da34f0c31c
Pavan Kumar Kallati <kallatipavankumar@gmail.com>
2020-12-11 21:32:21 +0530

    Added profile schema

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const port = process.env.PORT || 8080;
 const graphQLSchema = require('./graphql/schema');
 const graphQLResolver = require('./graphql/resolver');
 
+const Profile = require('./models/profile');
+const Post = require('./models/post');
+
 const server = new ApolloServer({
     typeDefs: graphQLSchema, resolvers: graphQLResolver
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
- 
+
+Profile.hasMany(Post);
 
 app.listen(port, () => console.log(`Apollo Server is listening to port: ${port}`));
--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,33 @@
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
 const Profile = sequelize.define('profile', {
-
+  userId: {
+    type: Sequelize.UUID,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  displayname: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+  },
+  verifiedAt: Sequelize.DATE,
+  role: {
+    type: Sequelize.ENUM,
+    values: ['ADMIN', 'USER', 'SYSTEM'],
+  },
+  status: {
+    type: Sequelize.ENUM,
+    values: ['VALID', 'BLOCKED']
+  },
+  moderatedBy: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
 });
 
 
