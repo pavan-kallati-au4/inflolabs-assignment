b465b504446f42bf14eb3d6eb9f7e9ea6eefbfba
Rishav Bharti <rishavbharti21@gmail.com>
2020-12-15 19:45:50 +0530

    Added email validator in Profile schema

--- models/profile.js
+++ models/profile.js
@@ -1,38 +1,44 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
 
-const Profile = sequelize.define('profile', {
-  userId: {
-    type: Sequelize.UUID,
-    allowNull: false,
-    primaryKey: true,
-    defaultValue: Sequelize.UUIDV4
+const Profile = sequelize.define(
+  "profile",
+  {
+    userId: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    displayName: Sequelize.STRING,
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    verifiedAt: Sequelize.DATE,
+    role: {
+      type: Sequelize.ENUM,
+      values: ["ADMIN", "USER", "SYSTEM"],
+      defaultValue: "USER",
+    },
+    status: {
+      type: Sequelize.ENUM,
+      values: ["VALID", "BLOCKED"],
+      defaultValue: "VALID",
+    },
+    moderatedBy: {
+      type: Sequelize.UUID,
+      allowNull: true,
+    },
   },
-  username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  displayName: Sequelize.STRING,
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  verifiedAt: Sequelize.DATE,
-  role: {
-    type: Sequelize.ENUM,
-    values: ['ADMIN', 'USER', 'SYSTEM'],
-    defaultValue: 'USER'
-  },
-  status: {
-    type: Sequelize.ENUM,
-    values: ['VALID', 'BLOCKED'],
-    defaultValue: 'VALID'
-  },
-  moderatedBy: {
-    type: Sequelize.UUID,
-    allowNull: true,
-  },
-}, { timestamps: true });
-
+  { timestamps: true }
+);
 
 module.exports = Profile;
