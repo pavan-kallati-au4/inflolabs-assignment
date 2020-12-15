516bb9228138cd07453544cd987a4861f8cdea08
Rishav Bharti <rishavbharti21@gmail.com>
2020-12-15 13:06:55 +0530

    Validators for email & existing user

--- graphql/resolvers/profile.js
+++ graphql/resolvers/profile.js
@@ -1,94 +1,117 @@
-const Profile = require('../../models/profile');
-const Report = require('../../models/report');
+const Profile = require("../../models/profile");
+const Report = require("../../models/report");
 const { Op } = require("sequelize");
-let { v4: uuid } = require('uuid');
+let { v4: uuid } = require("uuid");
 
 module.exports = {
+  Query: {
+    getAllReportedProfiles: async function (
+      parent,
+      { limit, skip },
+      context,
+      info
+    ) {
+      const reports = await Report.findAll({
+        limit,
+        offset: skip,
+        where: { reportedProfile: { [Op.ne]: null } },
+      });
+      if (!reports) {
+        const error = new Error("No Reported Profile Found");
+        error.code = 404;
+        throw error;
+      }
+      return reports;
+    },
+  },
 
-    Query: {
+  Mutation: {
+    reportProfile: async function (
+      parent,
+      { userId, description, reportedProfile }
+    ) {
+      const reporter = await Profile.findByPk(userId);
+      if (!reporter) {
+        const error = new Error("Invalid User");
+        error.code = 404;
+        throw error;
+      }
+      // console.log("REPORTER", reporter);
 
-        getAllReportedProfiles: async function (parent, { limit, skip }, context, info) {
-            const reports = await Report.findAll({ limit, offset: skip, where: { reportedProfile: { [Op.ne]: null } } });
-            if (!reports) {
-                const error = new Error('No Reported Profile Found');
-                error.code = 404;
-                throw error;
-            }
-            return reports;
-        },
+      const profile = await Profile.findByPk(reportedProfile);
+      if (!profile) {
+        const error = new Error("Invalid Reporting User");
+        error.code = 404;
+        throw error;
+      }
+      // console.log("PROFILE", profile)
+
+      const result = await profile.createReport({ userId, description });
+      if (!result) return false;
+      return true;
     },
 
+    moderateUser: async function (parent, { userId, status, moderatedBy }) {
+      if (userId === moderatedBy) {
+        const error = new Error("userId and Moderator Id can't be same");
+        error.code = 400;
+        throw error;
+      }
+      const user = await Profile.findByPk(userId);
+      if (!user) {
+        const error = new Error("No User Found");
+        error.code = 404;
+        throw error;
+      }
 
-    Mutation: {
-        reportProfile: async function (parent, { userId, description, reportedProfile }) {
-            const reporter = await Profile.findByPk(userId);
-            if (!reporter) {
-                const error = new Error('Invalid User');
-                error.code = 404;
-                throw error;
-            }
-            // console.log("REPORTER", reporter);
+      const moderator = await Profile.findByPk(moderatedBy);
+      if (!moderator) {
+        const error = new Error("No Moderator Found");
+        error.code = 404;
+        throw error;
+      }
 
-            const profile = await Profile.findByPk(reportedProfile);
-            if (!profile) {
-                const error = new Error('Invalid Reporting User');
-                error.code = 404;
-                throw error;
-            }
-            // console.log("PROFILE", profile)
+      if (moderator.role === "USER") {
+        const error = new Error("Invalid moderator");
+        error.code = 403;
+        throw error;
+      }
 
-            const result = await profile.createReport({ userId, description });
-            if (!result) return false;
-            return true;
-        },
-
-        moderateUser: async function (parent, { userId, status, moderatedBy }) {
-            if (userId === moderatedBy) {
-                const error = new Error("userId and Moderator Id can't be same");
-                error.code = 400;
-                throw error;
-            }
-            const user = await Profile.findByPk(userId);
-            if (!user) {
-                const error = new Error('No User Found');
-                error.code = 404;
-                throw error;
-            }
-
-            const moderator = await Profile.findByPk(moderatedBy);
-            if (!moderator) {
-                const error = new Error('No Moderator Found');
-                error.code = 404;
-                throw error;
-            }
-
-            if (moderator.role === 'USER') {
-                const error = new Error('Invalid moderator');
-                error.code = 403;
-                throw error;
-            }
-
-            const result = await user.update({ status, moderatedBy });
-            if (!result) return false;
-            return true;
-        },
-        //Filler function
-        createProfile: async function (parent, { username, displayName, email, role }) {
-            const userId = uuid();
-            const profile = await Profile.create({ userId, username, displayName, email, role });
-            const result = await profile.save()
-            if (!result) return false;
-            return true;
-        },
+      const result = await user.update({ status, moderatedBy });
+      if (!result) return false;
+      return true;
     },
+    //Filler function
+    createProfile: async function (
+      parent,
+      { username, displayName, email, role }
+    ) {
+      const userId = uuid();
+      const profileExists = await Profile.findOne({ email: email });
+      //   console.log(profileExists);
+      if (!profileExists) {
+        const profile = await Profile.create({
+          userId,
+          username,
+          displayName,
+          email,
+          role,
+        });
+        const result = await profile.save();
+        if (!result) return false;
+        return true;
+      } else {
+        throw new Error("Profile already present!");
+      }
+    },
+  },
 
-    ReportedProfile: {
-        profile: async function ({ reportedProfile }, args, context) {
-            return context.profileLoader.load(reportedProfile);
-        },
-        item: async function ({ reportedPost }, args, context) {
-            return context.postLoader.load(reportedPost);
-        },
-    }
-
+  ReportedProfile: {
+    profile: async function ({ reportedProfile }, args, context) {
+      return context.profileLoader.load(reportedProfile);
+    },
+    item: async function ({ reportedPost }, args, context) {
+      return context.postLoader.load(reportedPost);
+    },
+  },
 };
--- models/profile.js
+++ models/profile.js
@@ -1,37 +1,43 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
 
-const Profile = sequelize.define('profile', {
-  userId: {
-    type: Sequelize.UUID,
-    allowNull: false,
-    primaryKey: true,
+const Profile = sequelize.define(
+  "profile",
+  {
+    userId: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    displayName: Sequelize.STRING,
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    verifiedAt: Sequelize.DATE,
+    role: {
+      type: Sequelize.ENUM,
+      values: ["ADMIN", "USER", "SYSTEM"],
+      defaultValue: "USER",
+    },
+    status: {
+      type: Sequelize.ENUM,
+      values: ["VALID", "BLOCKED"],
+      defaultValue: "VALID",
+    },
+    moderatedBy: {
+      type: Sequelize.UUID,
+      allowNull: true,
+    },
   },
-  username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  displayName: Sequelize.STRING,
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  verifiedAt: Sequelize.DATE,
-  role: {
-    type: Sequelize.ENUM,
-    values: ['ADMIN', 'USER', 'SYSTEM'],
-    defaultValue: 'USER'
-  },
-  status: {
-    type: Sequelize.ENUM,
-    values: ['VALID', 'BLOCKED'],
-    defaultValue: 'VALID'
-  },
-  moderatedBy: {
-    type: Sequelize.UUID,
-    allowNull: true,
-  },
-}, { timestamps: true });
-
+  { timestamps: true }
+);
 
 module.exports = Profile;
